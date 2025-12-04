3df98af18bf2f3a1b26ee9aae977d28849e556f0
Tobias Petrich <tobias@rohrschacht.de>
2025-12-04 14:35:34 +0100

    add fonts

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -4,13 +4,20 @@
   config = {
     fonts.fontconfig.enable = true;
 
-    home.packages = with pkgs.nerd-fonts; [
+    home.packages = (with pkgs; [
+      noto-fonts
+      noto-fonts-extra
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      raleway
+    ]) ++ (with pkgs.nerd-fonts; [
       dejavu-sans-mono
       fira-code
       fira-mono
       hack
       inconsolata
       jetbrains-mono
-    ];
+    ]);
   };
 }
