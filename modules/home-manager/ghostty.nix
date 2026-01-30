6bdd0872ca323a1437efdc84e1a0bba1ada781e9
Tobias Petrich <tobias@rohrschacht.de>
2026-01-30 15:46:52 +0100

    ghostty: move to symlink

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -5,9 +5,6 @@
   config = {
     programs.ghostty.enable = true;
 
-    home.file.".config/ghostty/config" = {
-      source = ./configs/ghostty/config;
-      recursive = true;
-    };
+    home.file.".config/ghostty/config".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/modules/home-manager/configs/ghostty/config";
   };
 }
